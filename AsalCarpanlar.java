577e75c00fe395537057448612ba3234f79a0a48
Nurbusekaya <252c48djfm@privaterelay.appleid.com>
2026-05-31 22:24:11 +0300

    Update AsalCarpanlar.java

--- AsalCarpanlar.java
+++ AsalCarpanlar.java
@@ -9,23 +9,23 @@ public class AsalCarpanlar {
 
     }
 
-    // Asal çarpanları bulan metod
+
     public static List<Integer> AsalCarpanlariBul(int sayi) {
         List<Integer> asalCarpanlar = new ArrayList<>();
 
-        // 2 ile başla,çünkü 2 en küçük asal sayıdır
+        
         while (sayi % 2 == 0) {
             asalCarpanlar.add(2);
             sayi /= 2;
         }
-        // 3'ten başlayarak tek sayılarla devam et
+       
         for (int i = 3; i <= Math.sqrt(sayi); i += 2) {
             while (sayi % i == 0) {
                 asalCarpanlar.add(i);
                 sayi /= i;
             }
         }
-        // Eğer sayi hala büyük bir asal sayıysa,onu ekle
+    
         if (sayi > 2) {
             asalCarpanlar.add(sayi);
         }
